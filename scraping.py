a0c098fd0eb113105810989fb66da11742d1debc
Arianna Topbjerg <amt700@humboldt.edu>
2021-04-11 10:48:09 -0700

    update to challenge

--- scraping.py
+++ scraping.py
@@ -20,7 +20,7 @@ def scrape_all():
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
         "last_modified": dt.datetime.now(),
-        "hemisphere_image_urls": hemisphere_data(browser)
+        "hemisphere_image_urls": hemisphere_image_urls(browser)
     }
     # Ending automated browsing session
     browser.quit()
@@ -96,7 +96,7 @@ def mars_facts():
     return df.to_html()
 
 # Scraping hemisphere data
-def hemisphere_data(browser):
+def hemisphere_image_urls(browser):
     # 1. Use browser to visit the URL 
     url = 'https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars'
     browser.visit(url)
